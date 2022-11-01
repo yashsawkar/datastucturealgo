c80c0a7c911d6d449bf7a399f0c015213456f9c7
yashsawkar <sawkaryash257@gmail.com>
2022-11-01 21:23:08 +0530

    char arr

--- chararray.cpp
+++ chararray.cpp
@@ -6,6 +6,30 @@ using namespace std;
 
 int main()
 {
-     
+     int n;
+     cin >> n;
+     char sen[n+1];
+     cin >> sen;
+     cout << sen << endl;
+     bool con = 0;
+     for(int i = 0; i < n; i++)
+     {
+          if (sen[i]==sen[n-1-i])
+          {
+               con = 1;
+               break;
+          }
+          else { con = 0; }  
+     }
+
+     if(con)
+     {
+          cout << "It is palindrom.." << endl;
+     }
+     else
+     {    
+          cout <<" FALSE" << endl; 
+     }
      return 0;
 }
+ 
