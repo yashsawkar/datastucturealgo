66f9ef83de68758094be35ef8021f1c343845160
yashsawkar <sawkaryash257@gmail.com>
2022-11-05 10:39:07 +0530

    practicing linkedlist

---
+++ testlinked.cpp
@@ -0,0 +1,69 @@
+
+
+#include<iostream>
+using namespace std;
+
+class node{
+     public:
+     int data;
+     node*next;
+
+     node(int val)
+     {
+          data = val;
+          next =NULL;
+     }
+};
+
+
+void insertAThead(node*&head, int val)
+{
+     node*n = new node(val);
+     n->next = head;
+     head =n;
+}
+
+void insertATtail(node*&head, int val)
+{
+     node*n = new node(val);
+     if(head==NULL)
+     {
+          head=n;
+          return;
+     } 
+     node*temp = head;
+     while(temp->next!=NULL)
+     {
+          temp=temp->next;
+     }
+     temp->next = n;
+}
+
+
+void display(node*head)
+{
+     node*temp=head;
+     while(temp!=NULL)
+     {
+          cout << temp->data << " -> ";
+          temp = temp->next;
+     }
+     cout << "NULL" << endl;
+}
+
+int main()
+{
+     node*head=NULL;
+     for(int i=0;i<5;i++)
+     {
+          insertATtail(head,rand()%10);
+     }
+     display(head);
+     int val;
+     cin >> val;
+     insertAThead(head,val);
+     display(head);
+
+
+     return 0;
+}
