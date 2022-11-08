3bfe73adad27fe6ffa5a47beebf72cf6a9048a60
yashsawkar <sawkaryash257@gmail.com>
2022-11-08 23:06:10 +0530

    stack data structure

---
+++ stackDS.cpp
@@ -0,0 +1,75 @@
+
+
+#include<iostream>
+using namespace std;
+
+#define n 100
+
+class stack{
+     int *arr;
+     int top;
+     public:
+     stack()
+     {
+          arr = new int[n];
+          top = -1;
+     }
+
+     void push(int x)
+     {
+          if(top==n-1)
+          {
+               cout << "Stack overflow " << endl;
+               return;
+          }
+          top++;
+          arr[top] = x;
+     }
+
+     void pop()
+     {
+          if(top==-1)
+          {
+               cout << "No element to pop" << endl;
+               return;
+          }
+          top--;
+     }
+
+     int topie()
+     {
+          if(top==-1)
+          {
+               cout << "No element in stack" << endl;
+               return -1;
+          }
+          return arr[top];
+     }
+
+     bool empty()
+     {
+          return top==-1;
+     }
+};
+
+int main()
+{
+     stack st;
+     cout << st.empty() << endl;
+     for(int i = 0; i <6; i++)
+     {
+          st.push(rand()%10);
+     }
+     cout << st.topie() << endl;
+     st.pop();
+     cout << st.topie() << endl;
+     for (int i = 0; i < 5; i++)
+     {
+          st.pop();
+     }
+     st.pop();
+     cout << st.empty() << endl;
+     
+     
+     return 0;
+}
