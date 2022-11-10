a3c0d9d4623c08f48b3ad3e0d7886509e61f5097
yashsawkar <sawkaryash257@gmail.com>
2022-11-11 00:56:47 +0530

    reversing stack
    Reversing the stack with integers though recurrsion

--- reversestack.cpp
+++ reversestack.cpp
@@ -27,11 +27,55 @@ void reversingsentence(string s)
      cout << endl;
 }
 
+void insertATbottom(stack<int> &st,int ele)
+{
+     if(st.empty())
+     {
+          st.push(ele);
+          return;
+     }
+     int topele = st.top();
+     st.pop();
+     insertATbottom(st,ele);
+     st.push(topele);
+
+}
+
+void reverseinteger(stack<int> &st)
+{
+     if(st.empty())
+     {
+          return;
+     }
+
+     int leftover = st.top();
+     st.pop();
+     reverseinteger(st);
+     insertATbottom(st,leftover);
+}
 
 int main()
 {
-     string sen;
-     getline(cin,sen);
-     reversingsentence(sen);
+     // string sen;
+     // getline(cin,sen);
+     // reversingsentence(sen);
+
+     stack<int> st;
+     for(int i = 0; i < 5; i++)
+     {
+          int ele=rand()%10;
+          st.push(ele);
+          cout << ele << endl;
+     }
+
+     cout << " reversed stack is : " << endl;
+
+     while(!st.empty())
+     {
+          cout << st.top() << endl;
+          st.pop();
+     }
+     cout << endl;
+
      return 0;
 }
