f655c4c014b7b2254a2cfd18339c131f127218fb
yashsawkar <sawkaryash257@gmail.com>
2022-11-16 16:36:20 +0530

    infix stack evulation

--- expression.cpp
+++ expression.cpp
@@ -42,9 +42,69 @@ int PostFixEvulation(string s)
      return st.top(); 
 }
 
+int precident(char c)
+{
+     if ( c == '^')
+     {
+          return 3;
+     }else if (c == '*' && '/')
+     {
+          return 2;
+     }else if (c == '+' && '-')
+     {
+          return 1;
+     }else
+     {
+          return -1;
+     }
+}
+
+string InFixEvulation(string s)
+{
+     stack<char> st;
+     string res;
+
+     for(int i  = 0; i < s.length(); i++)
+     {
+          if((s[i] >= 'a' && s[i]<= 'z') || (s[i] >= 'A' && s[i]<= 'Z'))
+          {
+               res+= s[i];
+          }
+          else if(s[i]=='(')
+          {
+               st.push(s[i]);
+          }
+          else if(s[i]== ')')
+          {
+               while(!st.empty() && s[i] == '(')
+               {
+                    res += st.top();
+                    st.pop();
+               } 
+               if(!st.empty())
+               {
+                    st.pop(); 
+               }
+          }else{
+          while(!st.empty() && precident(st.top()) > precident(s[i]))
+          {
+               res += st.top();
+               st.pop();
+          }
+          st.push(s[i]);
+     }
+     while(!st.empty())
+     {
+          res += st.top();
+          st.pop(); 
+     }
+     } 
+     return res;
+}
 
 int main()
 {
-     cout << PostFixEvulation("23+") << endl;
+     // cout << PostFixEvulation("23+") << endl;
+     cout<<InFixEvulation("(a-b/c)*(a/k-1)")<<endl;
      return 0;
 }
