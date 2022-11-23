438146e34a7c8cf6a96e7b5f726db11887d48579
yashsawkar <sawkaryash257@gmail.com>
2022-11-23 12:56:57 +0530

    Quick sort

---
+++ Quicksort.cpp
@@ -0,0 +1,54 @@
+
+#include<iostream>
+using namespace std;
+
+void swap(int arr[], int f, int e)
+{
+     int temp = arr[e];
+     arr[e] = arr[f];
+     arr[f] = temp;
+}
+
+int partitation(int arr[], int l, int r)
+{
+     int pivot = arr[r];
+     int i = l-1;
+     for(int j = l; j < r; j++)
+     {
+          if(arr[j] < pivot)
+          {
+               i++;
+               swap(arr,i,j);
+          }
+     }
+     swap(arr,i+1,r);
+     return i+1;
+}
+
+
+void quicksort(int arr[], int l , int r)
+{
+     if(l<r)
+     {
+          int pi = partitation(arr,l,r);
+          quicksort(arr, l, pi-1);
+          quicksort(arr, pi+1, r);
+     }
+}
+
+
+
+int main()
+{    
+     int arr[10] = { 4,2,1,6,7,3,0,8,9,5};
+     cout << "array to be sorted : " << endl;
+     
+     cout << "Sorted array : "<< endl;
+     quicksort(arr,0,9);
+     for(int i = 0 ; i < 10; i++)
+     {
+          cout << arr[i] << endl;
+     }
+     
+     return 0;
+}
