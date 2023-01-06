edef478d7a1101ede45f17b3df9dbad5193258e1
yashsawkar <sawkaryash257@gmail.com>
2023-01-07 01:02:21 +0530

    preorder, inorder and postorder

--- binarytree.cpp
+++ binarytree.cpp
@@ -15,7 +15,41 @@ struct Node
         left = NULL;
         right = NULL;
     }
+};
+
+void preorder(struct Node* root)
+{
+    if(root== NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
 }
+
+void inorder(struct Node* node)
+{
+    if(node== NULL)
+    {
+        return;
+    }
+    inorder(node->left);
+    cout << node->data << " ";
+    inorder(node->right);
+}
+
+void postorder(struct Node* root)
+{
+    if(root== NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
 
 //     1
 //    / \
@@ -32,5 +66,12 @@ int main()
     root->left->right = new Node(5);
     root->right->left = new Node(6);
     root->right->right = new Node(7);
+    preorder(root);
+    cout << endl;
+    inorder(root);
+    cout << endl;
+    postorder(root);
+    cout << endl;
+    return 0;
 
 }
